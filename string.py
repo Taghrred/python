fd445e36b76de25530750b0999c22dcc0736aa20
Taghrred <60424553+Taghrred@users.noreply.github.com>
2020-03-02 11:32:33 +0200

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+# # # # # # fruit = 'banana'
+# # # # # # letter = fruit[1] 
+# # # # # # print(letter)
+# # # # # # x = 3
+# # # # # # w = fruit [x - 1]
+# # # # # # print(w)
+
+# # # # # a = 'book'
+# # # # # print(a[3])
+
+# # # # fruit = 'banana'
+# # # # print(len(fruit))
+
+# # # a = 'book'
+# # # print(len(a))
+
+# # a ='book'
+# # x = (len(a))
+# # print(x)
+
+# fruit = 'banana'
+# index = 0
+# while index < len(fruit):
+#     letter = fruit[index]
+#     print(index,letter)
+#     index = index + 1
