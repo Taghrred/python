d6938fbfaf7515aea33052d28a0e5b0e77380149
Taghrred <60424553+Taghrred@users.noreply.github.com>
2020-03-04 10:43:30 +0200

    Add files via upload

--- string.py
+++ string.py
@@ -24,3 +24,23 @@
 #     letter = fruit[index]
 #     print(index,letter)
 #     index = index + 1
+
+# word = input('whats word you need add?')
+# count = 0
+# T = input('whats you need letter!')
+# for letter in word :
+#     if letter == T :
+#         count = count + 1
+# print(count)        
+
+
+# s = 'Monty python'
+# print(s[0:4])
+# print(s[6:7])
+# print(s[6:20])
+
+# a = 'Hello'
+# b = a + 'There'
+# print(b)
+# c = a + ' ' + 'There'
+# print(c)
